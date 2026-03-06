9e69ffdc371fe7c998dcfed4e484d77466e0a922
Narendas26 <naosoft1419@gmail.com>
2026-03-07 04:31:54 +0600

    All Math Related Basic Topics Index

---
+++ math.js
@@ -0,0 +1,17 @@
+/**
+ * *******************************************************
+ *      Math Needed For Programming                       
+ * *******************************************************
+ * 1. Number: +ve, -ve, 0 [50, -9, 0]
+ * 2. integer: 2, 44, 88 [odd, even]
+ * 3. float : 5.6, 2.2
+ * 4. Operation: +, -, *, /
+ * 5. More Operation: % 
+ * 6. Percentage: 5%, 50 taka er 10%?
+ * 7, Sum: 14, 18, 16
+ * 8. Avg: 14, 18, 16
+ * 9. Max/Min: 14, 18, 16
+ * 10. Series: 
+ * 11. Area: Triangle 
+ * 12. Prime Number, Permutation e.t.c 
+ * **/
