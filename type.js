11bb7a8847e3ec83bbff0635b3eae67463b1be4b
Narendas26 <naosoft1419@gmail.com>
2026-03-07 18:09:28 +0600

    Type of Variable: String, Boolean, Number

---
+++ type.js
@@ -0,0 +1,34 @@
+//String Variable
+
+var name = "Naren Das"; 
+var address = "Banarupa Para, Bandarban";
+var country = 'Bangladesh';
+
+console.log(name);
+console.log(address);
+console.log(country);
+//For Type print"
+console.log(typeof name);
+console.log(typeof address);
+console.log(typeof country);
+
+
+//Boolean Variable
+
+var isRich = true;
+var passed = false;
+
+console.log(isRich);
+console.log(passed);
+//For Type print
+console.log(typeof isRich);
+console.log(typeof passed);
+
+
+//Number Variable
+
+var price = 500;
+
+console.log(price);
+//For Type print
+console.log(typeof price);
