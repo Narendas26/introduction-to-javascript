c6e33793f32a4cbef1edc181e72aaf06d3cf9633
Narendas26 <naosoft1419@gmail.com>
2026-03-07 17:09:12 +0600

    Variable Basic

---
+++ Variable.js
@@ -0,0 +1,23 @@
+/*
+    VAriable Concept
+    5 things to declare a variable:
+    1. var
+    2. variable-name
+    3. =
+    4. value
+    5. semecolon
+
+*/
+
+var weight = 38;
+var price = 38;
+var year = 1995;
+var money = 2005;
+money = 500;
+
+var age;
+console.log(age);
+
+console.log(money)
+console.log(price);
+
