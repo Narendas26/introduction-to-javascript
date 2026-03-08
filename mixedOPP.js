7cf22eb855438f38a0605a7d199850dff82bf1ef
Narendas26 <naosoft1419@gmail.com>
2026-03-09 00:38:39 +0600

    mixed

---
+++ mixedOPP.js
@@ -0,0 +1,12 @@
+var first = 'Naren';
+var second = ' Das';
+
+var fullName = first + second; //add two string
+console.log(fullName); 
+
+console.log(40 + 2);  //add two numbers
+
+console.log(40 + '2') //add one string and one number make it String.
+
+console.log('50' - 10); //Subtract one string from one number act as a number subtraction same thing happen for multiplication and division
+
