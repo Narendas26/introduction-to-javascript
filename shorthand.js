bcab0c8619895af50928aad430ef6b73b493f762
Narendas26 <naosoft1419@gmail.com>
2026-03-09 00:27:26 +0600

    Shorthand Rules

---
+++ shorthand.js
@@ -0,0 +1,19 @@
+// Shorthand Writing Rules":
+
+var price = 35;
+price = price + 10;
+console.log(price);
+
+price += 10;
+console.log(price);
+
+var fridge = 35000;
+
+fridge -= 1000;
+console.log(fridge);
+
+var orangePrice = 30;
+orangePrice *= 3;
+console.log(orangePrice);
+
+
